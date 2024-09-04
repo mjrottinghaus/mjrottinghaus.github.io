1eb134f0687fe38eb5b649825d8304c8726c437b
Matthew Rottinghaus <mrott0902@yahoo.com>
2024-09-03 21:56:05 -0600

    navbar work

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css"
+import { Navbar } from "./components/navbar/Navbar"
 
 function App() {
 
   return (
     <div className={styles.App}>
-      Hello World!
+      <Navbar/>
     </div>
   )
 }
